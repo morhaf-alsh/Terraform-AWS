37827f8709c07059fad316cd985b2d4177dbcc9e
morhaf-alsh <morhaf.alshoufi231@gmail.com>
2023-07-09 18:12:39 +0400

    terraform udemy

--- Backend/backend.tf
+++ Backend/backend.tf
@@ -3,8 +3,8 @@ terraform {
         key = "terraform/tfstate.tfstate"
         bucket = "morhaf"
         region = "us-east-1"
-        access_key = "AKIATMHF7HG3SLSDZVP4"
-        secret_key = "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"
+        access_key = ""
+        secret_key = ""
     }
 
 }
